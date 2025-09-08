aa5de462e946321169d2aee9e0b4417a2aa0d947
u8659 <u8659@C8659>
2025-09-08 16:54:37 +0800

    加入專案檔案。

---
+++ exercise/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace exercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
