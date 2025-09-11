6eebf09306e5fb605f648a339ac024e04c771143
ekko <koko911017@gmail.com>
2025-09-11 09:45:18 +0800

    增加角色登入設定

--- exercise/Controllers/HomeController.cs
+++ exercise/Controllers/HomeController.cs
@@ -1,19 +1,21 @@
-using System.Diagnostics;
+using exercise.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using exercise.Models;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 
 namespace exercise.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private string connectionString = @"Server=C8659\SQLEXPRESS;Database=MyGo;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+        //private string connectionString = @"Server=C8659\SQLEXPRESS;Database=MyGo;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+        private readonly IConfiguration _configuration;
 
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         // 首頁，顯示登入表單
@@ -24,7 +26,7 @@ namespace exercise.Controllers
 
         // 登入 POST 動作
         [HttpPost]
-        public IActionResult Login(string account, string pw)
+        public IActionResult Login(string account, string pw,string status)
         {
             if (string.IsNullOrEmpty(account) || string.IsNullOrEmpty(pw))
             {
@@ -34,15 +36,16 @@ namespace exercise.Controllers
 
             try
             {
+                string connectionString = _configuration.GetConnectionString("MyGoConnection");
                 using (SqlConnection conn = new SqlConnection(connectionString))
                 {
                     conn.Open();
-                    string sql = "SELECT COUNT(*) FROM Member WHERE account=@account AND pw=@pw";
+                    string sql = "SELECT COUNT(*) FROM Member WHERE account=@account AND pw=@pw AND status = @status";
                     using (SqlCommand cmd = new SqlCommand(sql, conn))
                     {
                         cmd.Parameters.AddWithValue("@account", account);
                         cmd.Parameters.AddWithValue("@pw", pw);
-
+                        cmd.Parameters.AddWithValue("@status", status);
                         int count = (int)cmd.ExecuteScalar();
 
                         if (count > 0)
